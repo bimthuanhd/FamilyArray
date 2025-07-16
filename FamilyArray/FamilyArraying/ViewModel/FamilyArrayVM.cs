095dd3b6d48550e994f4d9c93d46fa7e146de73e
bimthuanhd <99229276+bimthuanhd@users.noreply.github.com>
2025-07-16 11:47:20 +0700

    enh: update

--- FamilyArray/FamilyArraying/ViewModel/FamilyArrayVM.cs
+++ FamilyArray/FamilyArraying/ViewModel/FamilyArrayVM.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.DB;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,25 @@ namespace FamilyArraying.ViewModel
 {
     public class FamilyArrayVM : BaseViewModel
     {
-        private List<CurveModel> curves;
-        public List<CurveModel> Curves
+        private ObservableCollection<CurveModel> curves;
+        public ObservableCollection<CurveModel> Curves
         {
             get => curves;
             set
             {
                 curves = value;
                 OnPropertyChanged(nameof(curves));
+            }
+        }
+
+        private CurveModel selectedCurve;
+        public CurveModel SelectedCurve
+        {
+            get => selectedCurve;
+            set
+            {
+                selectedCurve = value;
+                OnPropertyChanged(nameof(selectedCurve));
             }
         }
 
