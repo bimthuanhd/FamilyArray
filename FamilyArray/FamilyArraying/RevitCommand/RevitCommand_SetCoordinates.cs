565ff6c7003bb07a8d310da726503becf7e26246
bimthuanhd <99229276+bimthuanhd@users.noreply.github.com>
2025-08-10 22:38:31 +0700

    update set coordinates

---
+++ FamilyArray/FamilyArraying/RevitCommand/RevitCommand_SetCoordinates.cs
@@ -0,0 +1,90 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.UI;
+using System.Windows;
+using System.Windows.Input;
+using SingleData;
+using FamilyArraying.ViewModel;
+using FamilyArraying.View;
+using System.Windows.Controls;
+
+namespace HTAddin
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class RevitCommand_SetCoordinates : IExternalCommand
+    {
+        private Data Data => Data.Instance;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Data.Instance.InitData(commandData);
+
+            try
+            {
+                IList<Reference> refs = Data.Sel.PickObjects(ObjectType.Element, "Chọn nhiều đối tượng");
+                if (refs == null || refs.Count == 0)
+                {
+                    TaskDialog.Show("Thông báo", "Không có đối tượng nào được chọn.");
+                    return Result.Cancelled;
+                }
+
+                var viewModel = new SetCoordinatesVM();
+
+                List<HashSet<string>> allParamNames = new List<HashSet<string>>();
+                foreach (var r in refs)
+                {
+                    HashSet<string> paramNames = new HashSet<string>();
+                    Element e = Data.Instance.Doc.GetElement(r);
+                    Group parentGroup = Data.Instance.Doc.GetElement(e.GroupId) as Group;
+                    if (parentGroup != null)
+                    {
+                        continue;
+                    }
+
+                    foreach (Parameter param in e.Parameters)
+                    {
+                        if (!param.IsReadOnly)
+                        {
+                            paramNames.Add(param.Definition.Name);
+                        }
+                    }
+
+                    if (paramNames.Count == 0) continue;
+
+                    allParamNames.Add(paramNames);
+                    viewModel.SelectedElements.Add(e);
+                }
+
+                HashSet<string> intersection = new HashSet<string>();
+                intersection = allParamNames[0];
+                for (int i = 1; i < allParamNames.Count; i++)
+                {
+                    intersection.IntersectWith(allParamNames[i]);
+                }
+
+                viewModel.Parameters = intersection.ToObservableCollection();
+                if (viewModel.Parameters.Count < 2)
+                {
+                    TaskDialog.Show("Thông báo", "Có ít hơn 2 parameters");
+                    return Result.Cancelled;
+                }
+                viewModel.SelectedParameter1 = viewModel.Parameters.FirstOrDefault(x => x == "Comments") ?? viewModel.Parameters.FirstOrDefault();
+                viewModel.SelectedParameter2 = viewModel.Parameters.FirstOrDefault(x => x == "Position") ?? viewModel.Parameters[1];
+
+                var view = new SetCoordinatesView() { DataContext = viewModel };
+                view.ShowDialog();
+            }
+            catch (Exception)
+            {
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ FamilyArray/FamilyArraying/ViewModel/SetCoordinatesVM.cs
@@ -0,0 +1,181 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using HTAddin;
+using Microsoft.SqlServer.Server;
+using SingleData;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using LinearArray = Autodesk.Revit.DB.LinearArray;
+using RadialArray = Autodesk.Revit.DB.RadialArray;
+using Frame = Autodesk.Revit.DB.Frame;
+using static Autodesk.Revit.DB.SpecTypeId;
+using Autodesk.Revit.UI;
+using System.Reflection.Emit;
+using FamilyArraying.Enum;
+using Autodesk.Revit.UI.Selection;
+using FamilyArraying.View;
+using Reference = Autodesk.Revit.DB.Reference;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace FamilyArraying.ViewModel
+{
+    public class SetCoordinatesVM : BaseViewModel
+    {
+        private ObservableCollection<string> parameters;
+        public ObservableCollection<string> Parameters
+        {
+            get => parameters;
+            set
+            {
+                parameters = value;
+                OnPropertyChanged(nameof(parameters));
+            }
+        }
+
+        private string selectedParameter1;
+        public string SelectedParameter1
+        {
+            get => selectedParameter1;
+            set
+            {
+                selectedParameter1 = value;
+                OnPropertyChanged(nameof(selectedParameter1));
+            }
+        }
+
+        private string selectedParameter2;
+        public string SelectedParameter2
+        {
+            get => selectedParameter2;
+            set
+            {
+                selectedParameter2 = value;
+                OnPropertyChanged(nameof(selectedParameter2));
+            }
+        }
+
+        private List<Element> selectedElements;
+        public List<Element> SelectedElements
+        {
+            get => selectedElements ?? (selectedElements = new List<Element>());
+            set
+            {
+                selectedElements = value;
+                OnPropertyChanged(nameof(selectedElements));
+            }
+        }
+
+        #region Command
+        public ICommand OkCmd { get; set; }
+        public ICommand CancelCmd { get; set; }
+        #endregion
+
+        public void BtnOkeCommand(object window)
+        {
+            (window as Window).Close();
+
+            try
+            {
+                // check 2 Cbbs must be different together
+                if (SelectedParameter1 == SelectedParameter2)
+                {
+                    TaskDialog.Show("Revit", "2 parameters must be different!");
+                    return;
+                }
+
+                //using (Transaction tx = new Transaction(Data.Instance.Doc, "Set Coordinates"))
+                //{
+                //    tx.Start();
+                //    foreach (var element in selectedElements)
+                //    {
+                //        if (element.Location is LocationPoint locationPoint)
+                //        {
+                //            XYZ sharedPoint = Data.Instance.Doc.ActiveProjectLocation.GetTotalTransform().OfPoint(locationPoint.Point);
+                //            Parameter paramX = element.LookupParameter(SelectedParameter1);
+                //            Parameter paramY = element.LookupParameter(SelectedParameter2);
+
+                //            double xInMeters = UnitUtils.ConvertFromInternalUnits(sharedPoint.X, UnitTypeId.Meters);
+                //            double yInMeters = UnitUtils.ConvertFromInternalUnits(sharedPoint.Y, UnitTypeId.Meters);
+
+                //            if (paramX != null && !paramX.IsReadOnly)
+                //            {
+                //                paramX.Set(xInMeters.ToString());
+                //            }
+
+                //            if (paramY != null && !paramY.IsReadOnly)
+                //            {
+                //                paramY.Set(yInMeters.ToString());
+                //            }
+                //        }
+                //    }
+
+                //    tx.Commit();
+                //}
+
+                using (var progressBarView = new ProgressBarView($"Set Coordinates {selectedElements.Count}"))
+                {
+                    using (TransactionGroup transactionGroup = new TransactionGroup(Data.Instance.Doc))
+                    {
+                        transactionGroup.Start("Group Set Coordinates");
+
+                        progressBarView.Run(selectedElements, (element) =>
+                        {
+                            using (Transaction transaction = new Transaction(Data.Instance.Doc))
+                            {
+                                transaction.Start("Group Set Coordinates");
+                                if (element.Location is LocationPoint locationPoint)
+                                {
+                                    XYZ sharedPoint = Data.Instance.Doc.ActiveProjectLocation.GetTotalTransform().OfPoint(locationPoint.Point);
+                                    Parameter paramX = element.LookupParameter(SelectedParameter1);
+                                    Parameter paramY = element.LookupParameter(SelectedParameter2);
+
+                                    double xInMeters = UnitUtils.ConvertFromInternalUnits(sharedPoint.X, UnitTypeId.Meters);
+                                    double yInMeters = UnitUtils.ConvertFromInternalUnits(sharedPoint.Y, UnitTypeId.Meters);
+
+                                    if (paramX != null && !paramX.IsReadOnly)
+                                    {
+                                        paramX.Set(xInMeters.ToString());
+                                    }
+
+                                    if (paramY != null && !paramY.IsReadOnly)
+                                    {
+                                        paramY.Set(yInMeters.ToString());
+                                    }
+                                }
+                                transaction.Commit();
+                            }
+                        });
+
+                        if (progressBarView.IsClosed)
+                            transactionGroup.RollBack();
+                        else
+                            transactionGroup.Assimilate();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+        public void BtnCancelCommand(object window)
+        {
+            (window as Window).Close();
+        }
+
+        public SetCoordinatesVM()
+        {
+            OkCmd = new RelayCommand(BtnOkeCommand);
+            CancelCmd = new RelayCommand(BtnCancelCommand);
+        }
+    }
+}
